138d36d255ab9a302dec45f4cba701c2418ddbf8
Rocco Mannino <RWMannio@gmail.com>
2022-03-09 20:29:22 -0500

    Final

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-var rif18O = (function() {
-    var a = randInt(1,8)
-    return a;
-})(); 
-
 //4
 function placehold() {
     return location.reload();
@@ -29,33 +24,15 @@ document.getElementById("inpWager").addEventListener('click', function(){
     document.getElementById('gamedsp').textContent =`${randInt(0,9)}`
     //let numberDSP = Number(document.getElementById("gamedsp").value);
 
-
     document.getElementById('numbers47').addEventListener('click', function() {
-        //5
-        var rif47O = (function() {
-            var a = randInt(4,7)
-            return a;
-        })();
-        
-        let numberDSP = rif47O;
+        var numberDSP = randInt(16,22);
         console.log(numberDSP)
         document.getElementById('gamedsp').textContent =`${numberDSP}`
 
-        if (numberDSP >= '22') {
-            let money = wager*-1;
-            console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
-            let conext = window.confirm("Busted! Do You wish to try again?")
-            if (conext === 'true') {
-                placehold()
-                //close(window)
-            } else if (conext === 'false') {
-                close(window)
-            }
-        } else if (numberDSP === '16') {
+        if (numberDSP === '16') {
             let money = wager*0.25;
             console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            document.getElementById("moneydsp").textContent = `${'$'+ money}`
             let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
             if (conext === 'true') {
                 close(window)
@@ -66,7 +43,7 @@ document.getElementById("inpWager").addEventListener('click', function(){
         } else if (numberDSP === '17') {
             let money = wager*0.50;
             console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            document.getElementById("moneydsp").textContent = `${'$'+ money}`
             let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
             if (conext === 'true') {
                 close(window)
@@ -77,7 +54,7 @@ document.getElementById("inpWager").addEventListener('click', function(){
         } else if (numberDSP === '18') {
             let money = wager*0.75;
             console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            document.getElementById("moneydsp").textContent = `${'$'+ money}`
             let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
             if (conext === 'true') {
                 close(window)
@@ -88,7 +65,7 @@ document.getElementById("inpWager").addEventListener('click', function(){
         } else if (numberDSP === '19') {
             let money = wager*1;
             console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            document.getElementById("moneydsp").textContent = `${'$'+ money}`
             let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
             if (conext === 'true') {
                 close(window)
@@ -99,94 +76,102 @@ document.getElementById("inpWager").addEventListener('click', function(){
         } else if (numberDSP === '20') {
             let money = wager*1.50;
             console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            document.getElementById("moneydsp").textContent = `${'$'+ money}`
             win()
         } else if (numberDSP === '21') {
+            let money = wager*2;
+            console.log(money)
+            document.getElementById("moneydsp").textContent = `${'$'+ money}`
+            win()
+        } else if (numberDSP >= '22') {
+            let money = wager*-1;
+            console.log(money)
+            document.getElementById("moneydsp").textContent = `${'$'+ money}`
+            let conext = window.confirm("Busted! Do You wish to try again?")
+            if (conext === 'true') {
+                placehold()
+                //close(window)
+            } else if (conext === 'false') {
+                close(window)
+            }
+        }
+    })
+    /*
+    document.getElementById('numbers18').addEventListener('click', function() { 
+        var numbersDSP = randInt(1,8);
+        console.log(numbersDSP)
+        document.getElementById('gamedsp').textContent =`${numbersDSP}`
+
+        if (numbersDSP >= '22') {
+            let money = wager*-1;
+            console.log(money)
+            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            let conext = window.confirm("Busted! Do You wish to try again?")
+            if (conext === 'true') {
+                placehold()
+                //close(window)
+            } else if (conext === 'false') {
+                close(window)
+            }
+        } else if (numbersDSP === '16') {
+            let money = wager*0.25;
+            console.log(money)
+            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
+            if (conext === 'true') {
+                close(window)
+            } else if (conext === 'false') {
+                placehold()
+                //close(window)
+            }
+        } else if (numbersDSP === '17') {
+            let money = wager*0.50;
+            console.log(money)
+            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
+            if (conext === 'true') {
+                close(window)
+            } else if (conext === 'false') {
+                placehold()
+                //close(window)
+            }
+        } else if (numbersDSP === '18') {
+            let money = wager*0.75;
+            console.log(money)
+            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
+            if (conext === 'true') {
+                close(window)
+            } else if (conext === 'false') {
+            placehold()
+                //close(window)
+            }
+        } else if (numbersDSP === '19') {
+            let money = wager*1;
+            console.log(money)
+            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
+            if (conext === 'true') {
+                close(window)
+            } else if (conext === 'false') {
+                placehold()
+                //close(window)
+            }
+        } else if (numbersDSP === '20') {
+            let money = wager*1.50;
+            console.log(money)
+            document.getElementById("moneyDSP").textContent = `${'$'+money}`
+            win()
+
+        } else if (numbersDSP === '21') {
             let money = wager*2;
             console.log(money)
             document.getElementById("moneyDSP").textContent = `${'$'+money}`
             win()
         }
-    })
     
-    document.getElementById('numbers18').addEventListener('click', function() {
-        var rif18O = (function() {
-            var a = randInt(1,8)
-            return a;
-        })(); 
-        
-        document.getElementById('gamedsp').textContent =`${rif18O}`
-        console.log(rif18O)
-        let numberDSP = rif18O;
-
-        if (numberDSP >= '22') {
-            let money = wager*-1;
-            console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
-            let conext = window.confirm("Busted! Do You wish to try again?")
-            if (conext === 'true') {
-                placehold()
-                //close(window)
-            } else if (conext === 'false') {
-                close(window)
-            }
-        } else if (numberDSP === '16') {
-            let money = wager*0.25;
-            console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
-            let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
-            if (conext === 'true') {
-                close(window)
-            } else if (conext === 'false') {
-                placehold()
-                //close(window)
-            }
-        } else if (numberDSP === '17') {
-            let money = wager*0.50;
-            console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
-            let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
-            if (conext === 'true') {
-                close(window)
-            } else if (conext === 'false') {
-                placehold()
-                //close(window)
-            }
-        } else if (numberDSP === '18') {
-            let money = wager*0.75;
-            console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
-            let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
-            if (conext === 'true') {
-                close(window)
-            } else if (conext === 'false') {
-            placehold()
-                //close(window)
-            }
-        } else if (numberDSP === '19') {
-            let money = wager*1;
-            console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
-            let conext = window.confirm("You've won $"+ money + "Do You wish to continue gambling?")
-            if (conext === 'true') {
-                close(window)
-            } else if (conext === 'false') {
-                placehold()
-                //close(window)
-            }
-        } else if (numberDSP === '20') {
-            let money = wager*1.50;
-            console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
-            win()
-
-        } else if (numberDSP === '21') {
-            let money = wager*2;
-            console.log(money)
-            document.getElementById("moneyDSP").textContent = `${'$'+money}`
-            win()
-        }
     })
+    */
 
     /*
     //3
